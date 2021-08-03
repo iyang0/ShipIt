870a4fff2f706cff2c119f1b3400a0a9fcdb49b4
magus0 <harvestashya@gmail.com>
2021-08-03 16:37:24 -0700

    added schema for validation and validate the route

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const router = new express.Router();
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/orderSchema.json");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -14,7 +16,14 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
+  const result = jsonschema.validate(req.body, orderSchema);
+  if (!result.valid) {
+    // pass validation errors to error handler
+    //  (the "stack" key is generally the most useful)
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+  let { productId, name, addr, zip } = req.body
   const shipId = await shipProduct({ productId, name, addr, zip });
   return res.json({ shipped: shipId });
 });
